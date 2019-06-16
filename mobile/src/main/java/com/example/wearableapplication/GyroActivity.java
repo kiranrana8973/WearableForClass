b94e2caee78bfe7b7316e4baa51d9e3e87bc401d
Kiran Rana <kiranrana8973@gmail.com>
2019-06-16 16:49:07 +0545

    changed gyro

--- mobile/src/main/java/com/example/wearableapplication/GyroActivity.java
+++ mobile/src/main/java/com/example/wearableapplication/GyroActivity.java
@@ -17,25 +17,28 @@ public class GyroActivity extends AppCompatActivity {
         sensorInstance();
 
     }
-    SensorEventListener gyrolistener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            if (event.values[2] > 0.5f){
-                Toast.makeText(GyroActivity.this, "left", Toast.LENGTH_SHORT).show();
-            }
-            else if (event.values[2] < -0.5f){
-                Toast.makeText(GyroActivity.this, "right", Toast.LENGTH_SHORT).show();
-            }
-        }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
     private void sensorInstance(){
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        sensorManager.registerListener(gyrolistener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+        SensorEventListener gyrolistener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.values[1] < 0){
+                    Toast.makeText(GyroActivity.this, "left", Toast.LENGTH_SHORT).show();
+                }
+                else if (event.values[1] > 0){
+                    Toast.makeText(GyroActivity.this, "right", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        sensorManager.registerListener(gyrolistener,sensor,SensorManager.SENSOR_DELAY_FASTEST);
     }
 }
